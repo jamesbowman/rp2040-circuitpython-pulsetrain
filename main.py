95ebb2f0db0f0b49f5418b1eb378a348b6560e64
James Bowman <jamesb@excamera.com>
2026-04-27 12:59:28 -0700

    pc-based interpretation

--- main.py
+++ main.py
@@ -14,21 +14,47 @@ from rp2pio import StateMachine
 SWIO_PIN = board.GP0
 PIO_FREQUENCY = 1_000_000
 
-swio_tx = Program(
-    """
-.program swio_tx
+src0 = """
+.program pt
 
 main:
-    pull block; 
-    mov x, osr
-delay:
-    jmp x-- delay
+    pull block
     set pins, 0
     set pins, 1
     jmp main
+public set0:
+public set1:
 """
-)
-for (i,opcode) in enumerate(swio_tx.assembled):
+
+
+next = """
+    pull block
+    mov pc, osr
+"""
+src1 = f"""
+.program pt
+.origin 0
+
+main:
+{next}
+
+public set0:
+    set pins, 0
+{next}
+
+public set1:
+    set pins, 1
+{next}
+"""
+
+src = src1
+
+print(src)
+pt = Program(src)
+l = pt.public_labels
+
+rlabels = {o:nm for (nm, o) in l.items()}
+for (i,opcode) in enumerate(pt.assembled):
     bb = f"{opcode:016b}"
     insn    = 0x07 & (opcode >> 13)
     delay   = 0x1f & (opcode >> 8)
@@ -44,17 +70,22 @@ for (i,opcode) in enumerate(swio_tx.assembled):
         dis = f"mov {dst} {src}"
     else:
         dis = ['jmp', 'wait', 'in', 'out', 'pushpull', 'mov', 'irq', 'set'][insn]
+    if i in rlabels:
+        print(rlabels[i] + ":")
     print(f"{i:2d} {insn:03b}_{delay:05b}_{f1:03b}_{f2:05b}    {dis}")
+print()
 
 sm = StateMachine(
-    swio_tx.assembled,
+    pt.assembled,
     frequency=PIO_FREQUENCY,
     first_set_pin=SWIO_PIN,
     set_pin_count=1,
     fifo_type="tx",
+    **pt.pio_kwargs
 )
 
-delay_words = array.array("I", [0] * 100)
+delay_words = array.array("I", [l['set0'], l['set1']] * 100)
+print(f"{delay_words=}")
 
 while True:
     sm.write(delay_words)
